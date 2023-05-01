4e829463d0d1a3bc4fffdf7614fd6bae512a0ca4
javiser <javiser@users.noreply.github.com>
2023-05-01 12:06:45 +0200

    Deactivate MQTT by default, see issue #6

--- src/mqtt_config.hpp
+++ src/mqtt_config.hpp
@@ -2,7 +2,7 @@
 #define _INCLUDE_MQTT_CONFIG_HPP_
 
 // Comment or delete this line if you don't want the MQTT functionality be compiled and used
-#define MQTT_ACTIVE
+//#define MQTT_ACTIVE
 
 #ifdef MQTT_ACTIVE
 #define MQTT_BROKER_ADDRESS "mqtt://192.168.178.29:1883"
